9277f4dd6b31b66862fb17c973c8e80e712a5fee
James Lavin <james@jameslavin.com>
2020-12-06 13:52:09 -0500

    empty Elixir app

---
+++ lib/ls_escript.ex
@@ -0,0 +1,18 @@
+defmodule LsEscript do
+  @moduledoc """
+  Documentation for `LsEscript`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LsEscript.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ls_escript_test.exs
@@ -0,0 +1,8 @@
+defmodule LsEscriptTest do
+  use ExUnit.Case
+  doctest LsEscript
+
+  test "greets the world" do
+    assert LsEscript.hello() == :world
+  end
+end
